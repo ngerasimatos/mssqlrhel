480c1d1d5f146bd2d9febbdda4c36e4566432008
ngerasimatos <nicholas.gerasimatos@gmail.com>
2018-02-05 14:29:56 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-oc new-project mssql 
-oc adm policy add-scc-to-user anyuid -z default
-oc new-app https://github.com/ngerasimatos/mssqlrhel 
-oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123!
-oc volume "dc/mssqlrhel" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=mssqlrhel --claim-mode="ReadWriteOnce" --claim-size=1G
+oc new-project mssql --as system:admin
+oc adm policy add-scc-to-user anyuid -z default --as system:admin
+oc new-app https://github.com/ngerasimatos/mssqlrhel  --as system:admin
+oc env "dc/mssqlrhel" -e ACCEPT_EULA=y -e SA_PASSWORD=RedHat123! --as system:admin
+oc volume "dc/mssqlrhel" --add --mount-path=/var/opt/mssql --type=persistentVolumeClaim --claim-name=mssqlrhel --claim-mode="ReadWriteOnce" --claim-size=1G --as system:admin
 sleep 20
-oc logs -f bc/mssqlrhel
+oc logs -f bc/mssqlrhel --as system:admin
 
